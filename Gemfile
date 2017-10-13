c838906c59f373e7db9f44d9e36321bade28e82b
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-10-13 00:43:01 -0400

    working logingit add -A

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ end
 gem 'rails', '~> 5.1.4'
 # Use Puma as the app server
 gem 'bootstrap-sass', '3.3.7'
+gem 'jquery-rails'
 gem 'bcrypt',         '3.1.11'
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,17 @@ class UsersController < ApplicationController
   def show
   @user = User.find(params[:id])
 end
-def create
-  @user = User.new(user_params)
-  if @user.save
-    flash[:success] = "Welcome to the Sample App!"
-    redirect_to @user
-  else
-    render 'new'
-  end
-end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 private
 
   def user_params
