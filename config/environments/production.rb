1faf7452292e4e145688c573b70ba41ab111bd6b
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-10-13 00:23:56 -0400

    use ssl and puma webserver

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Project_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+ config.force_ssl = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
