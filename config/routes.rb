d1983aa29221f4a39b6580115c43d06db0782a5f
Abhishek Kulkarni <abrkulkarni@gmail.com>
2017-10-12 23:28:24 -0400

    Add pry gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'sessions/new'
+
+  get 'users/new'
   resources :users
-  root 'application#display_map'
+  ##get '/displaymap' to: 'application#display_map'
+  root 'static_pages#login'
+  get  '/choosetype',    to: 'static_pages#choosetype'
+  get  '/signup',  to: 'users#new'
+  get   '/login',   to: 'sessions#new'
+post   '/login',   to: 'sessions#create'
+delete '/logout',  to: 'sessions#destroy'
+
+
 end
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get sessions_new_url
     assert_response :success
   end
 
