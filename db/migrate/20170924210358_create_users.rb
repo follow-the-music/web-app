a4fbdf9606ee593e9f8994c27a4d73ff2ce28a95
denizamadoba <damado@brandeis.edu>
2017-10-13 14:49:02 -0400

    migrate

--- db/migrate/20170924210358_create_users.rb
+++ db/migrate/20170924210358_create_users.rb
@@ -4,13 +4,13 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     change_table :users do |t|
       t.string :name
       t.string :email
-      t.string :city
-      t.string :genre
-      t.string :instruments
-      t.string :fav_artist
-      t.string :playlist
-      t.string :links
-      t.string :intro_message
+      # t.string :city
+      # t.string :genre
+      # t.string :instruments
+      # t.string :fav_artist
+      # t.string :playlist
+      # t.string :links
+      # t.string :intro_message
 
       t.timestamps
     end
