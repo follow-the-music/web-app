792f1580ad22c2c194e002ce1aa813fba90560db
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-10-13 00:20:50 -0400

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get   '/login',   to: 'sessions#new'
 post   '/login',   to: 'sessions#create'
 delete '/logout',  to: 'sessions#destroy'
+post '/signup',  to: 'users#create'
  resources :users
 
 
