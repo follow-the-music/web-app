0c1b7ce776614089cb0b039b68c188b8841d8c9b
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-10-20 16:05:54 -0400

    fixme3

--- db/migrate/20171013211359_create_jam_sessions.rb
+++ db/migrate/20171013211359_create_jam_sessions.rb
@@ -1,8 +1,6 @@
 class CreateJamSessions < ActiveRecord::Migration[5.1]
   def change
     create_table :jam_sessions do |t|
-      t.float :longitude
-      t.float :latitude
       t.integer :host_id
       t.integer :max_players
       t.integer :max_listeners
