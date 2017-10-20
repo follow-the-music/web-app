2d99545831acfbc420abaf0fcef78f96d65f7016
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-10-20 15:55:09 -0400

    fixing user migration

--- db/migrate/20170924210358_create_users.rb
+++ db/migrate/20170924210358_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     # add_column :users, :city, :string
-    change_table :users do |t|
+    create_table :users do |t|
       t.string :name
       t.string :email
 
