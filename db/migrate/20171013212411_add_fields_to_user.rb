d3d35b567d664ab5a05398537ad4851af4ab2439
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-10-20 16:01:49 -0400

    fixing migrations 2

--- db/migrate/20171013212411_add_fields_to_user.rb
+++ db/migrate/20171013212411_add_fields_to_user.rb
@@ -1,7 +1,6 @@
 class AddFieldsToUser < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :user_name, :text
-    add_column :users, :picture, :bitmap
     add_column :users, :biography, :text
     add_column :users, :rating, :float
     add_column :users, :longitude, :float
