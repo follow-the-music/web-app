63590cce9880b6416289df5060bf4882595b6bab
Abhishek Kulkarni <abrkulkarni@gmail.com>
2017-10-26 17:55:02 -0400

    Add model tests for users pertaining to passwords

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,4 +12,20 @@ end
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test "password should be present" do
+   user = User.new(name: "Example1", email: "example1@user.com")
+   assert_not user.valid?
+ end
+
+ test "password should be at least 6 characters long" do
+   user = User.new(name: "Example2", email: "example2@user.com", password: "p", password_confirmation: "p")
+   assert_not user.valid?
+ end
+
+ test "passwords and confirmation should match" do
+   user = User.new(name: "Example3", email: "example3@user.com", password: "password", password_confirmation: "not_password")
+   assert_not user.valid?
+ end
+
 end
