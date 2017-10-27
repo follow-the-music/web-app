834dd45694a13a75a15b85ecffe2f194a887a705
Abhishek Kulkarni <abrkulkarni@gmail.com>
2017-10-27 16:17:57 -0400

    Add chat message scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020204447) do
+ActiveRecord::Schema.define(version: 20171027201303) do
+
+  create_table "chat_messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "author_id"
+    t.integer "jam_session_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "guest_session_associations", force: :cascade do |t|
     t.integer "user_id"
