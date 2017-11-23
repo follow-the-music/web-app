9ca561a8db55d11c373cc4ccc4dd377334959cf7
Abhishek Kulkarni <abrkulkarni@gmail.com>
2017-11-22 21:00:11 -0500

    Removed redundant display_map method and modified modal IDs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     include SessionsHelper
 
-  def display_map
-    @jam_sessions = JamSession.all.order(:name).paginate(:page => params[:page])
-  end
-  # def check(current_session)
-  #   @not_in_jam_session=GuestSessionAssociation.where(:jam_session_id=>current_session.id, :user_id=> session[:user_id]).nil?
-  #   @not_in_jam_session
-  # end
-
   def choice
     # write the code to set this with the choice button
     session[:player]=true
