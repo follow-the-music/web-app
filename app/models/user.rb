4f3e2f6c5fc76d86b461f5aa9ed7d09778a4d271
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-12-01 14:43:27 -0500

    add chat to yoursessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "50x50>" }, default_url: ":style/defaultprofile.jpg"
 
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
