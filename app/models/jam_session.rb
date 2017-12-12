084f55c57df83c0291efc9563066142080c4c809
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-12-12 00:12:34 -0500

    card fix

--- app/models/jam_session.rb
+++ app/models/jam_session.rb
@@ -3,7 +3,7 @@ class JamSession < ApplicationRecord
   has_many :guest_session_associations
   geocoded_by :address
   after_validation :geocode
-  self.per_page = 6
+  self.per_page = 5
   # filterrific(
   #   available_filters: [
   #     :search_query_address,
