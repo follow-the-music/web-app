1e6b3f2ae8334548058608d747cd60aaae257775
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-12-15 22:11:37 -0500

    aws fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
   Paperclip.options[:command_path] = "/usr/local/bin/"
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :bucket => 'follow-the-music-develop'
+}
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
