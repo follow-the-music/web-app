c5106ebf11bf2de399aeeba38bc314e544c5743e
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-12-15 22:54:32 -0500

    Aws fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,12 +13,14 @@ Rails.application.configure do
   config.consider_all_requests_local = true
   Paperclip.options[:command_path] = "/usr/local/bin/"
   config.paperclip_defaults = {
-  :storage => :s3,
-  :bucket => ENV['AWS_BUCKET'],
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-  :s3_region => ENV.fetch('AWS_REGION')
-}
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
 }
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,13 +4,15 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
     Paperclip.options[:command_path] = "/usr/local/bin/"
-  config.paperclip_defaults = {
-  :storage => :s3,
-  :bucket => ENV['AWS_BUCKET'],
-  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-  :s3_region => ENV.fetch('AWS_REGION')
-}
+    config.paperclip_defaults = {
+      storage: :s3,
+      s3_credentials: {
+        bucket: ENV.fetch('S3_BUCKET_NAME'),
+        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        s3_region: ENV.fetch('AWS_REGION'),
+      }
+    }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
