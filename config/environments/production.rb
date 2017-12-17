e400e3d79d9d7ab714ba0a903c788e68970b32be
Lizzie Koshelev <lizkosheleva@gmail.com>
2017-12-16 22:54:09 -0500

    hi

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,11 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-    
+
     config.paperclip_defaults = {
       storage: :s3,
       s3_credentials: {
-        bucket: ENV.fetch('S3_BUCKET_NAME'),
+        bucket: ENV.fetch('AWS_BUCKET'),
         access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
         secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
         s3_region: ENV.fetch('AWS_REGION'),
