655aefd8494ac1e7258b5158dd5d97923b91b308
Abhishek Kulkarni <abrkulkarni@gmail.com>
2017-12-17 11:17:46 -0500

    Fixed join jam session redirect issue

--- app/controllers/guest_session_associations_controller.rb
+++ app/controllers/guest_session_associations_controller.rb
@@ -4,32 +4,38 @@ class GuestSessionAssociationsController < ApplicationController
   # GET /guest_session_associations
   # GET /guest_session_associations.json
   def index
+    puts "\n\n\n\n\t\t\t***  INDEX  ***\n\n\n\n"
     @guest_session_associations = GuestSessionAssociation.where(user_id:[session[:user_id]])
   end
 
   # GET /guest_session_associations/1
   # GET /guest_session_associations/1.json
   def show
-    @jam_session=JamSession.find(@guest_session_association.jam_session_id)
+    puts "\n\n\n\n\t\t\t***  SHOW  ***\n\n\n\n"
+    @jam_session= JamSession.find(@guest_session_association.jam_session_id)
     @host_name = User.where(id:@jam_session.host_id).pluck(:name)[0]
     @users= User.where(id: GuestSessionAssociation.where(jam_session_id: @jam_session.id).distinct.pluck(:user_id))
     @jam_players_count= GuestSessionAssociation.where(jam_session_id: @jam_session.id,player:true).distinct.count
     @jam_listeners_count= GuestSessionAssociation.where(jam_session_id: @jam_session.id,player:false).distinct.count
     @chat_messages = ChatMessage.where(jam_session_id: @jam_session.id).sort_by { |message| message.created_at}
+    redirect_to jam_sessions_path
   end
 
   # GET /guest_session_associations/new
   def new
+    puts "\n\n\n\n\t\t\t***  NEW  ***\n\n\n\n"
     @guest_session_association = GuestSessionAssociation.new
   end
 
   # GET /guest_session_associations/1/edit
   def edit
+    puts "\n\n\n\n\t\t\t***  EDIT  ***\n\n\n\n"
   end
 
   # POST /guest_session_associations
   # POST /guest_session_associations.json
   def create
+    puts "\n\n\n\n\t\t\t***  CREATE  ***\n\n\n\n"
     @jam_session=JamSession.find(@guest_session_association.jam_session_id)
     @users = User.where(id: GuestSessionAssociation.where(jam_session_id: @jam_session.id).distinct.pluck(:user_id))
     @players = User.where(id: GuestSessionAssociation.where(jam_session_id: @jam_session.id, player:true).distinct.pluck(:user_id))
@@ -53,8 +59,7 @@ class GuestSessionAssociationsController < ApplicationController
           end
         end
       else
-        format.html { redirect_to @guest_session_association, notice: 'Session is full.' }
-        format.json { render :show, status: :unprocessable_entity, location: @guest_session_association }
+        format.html { redirect_to jam_sessions_path }
      end
     else
       @jam_listeners_count= GuestSessionAssociation.where(jam_session_id: @jam_session.id,player:false).distinct.count
@@ -77,6 +82,7 @@ class GuestSessionAssociationsController < ApplicationController
   # PATCH/PUT /guest_session_associations/1
   # PATCH/PUT /guest_session_associations/1.json
   def update
+    puts "\n\n\n\n\t\t\t***  UPDATE  ***\n\n\n\n"
     respond_to do |format|
       if @guest_session_association.update(guest_session_association_params)
         format.html { redirect_to @guest_session_association, notice: 'Guest session association was successfully updated.' }
@@ -91,7 +97,7 @@ class GuestSessionAssociationsController < ApplicationController
   # DELETE /guest_session_associations/1
   # DELETE /guest_session_associations/1.json
   def destroy
-
+    puts "\n\n\n\n\t\t\t***  DESTROY  ***\n\n\n\n"
     @guest_session_association.destroy
     respond_to do |format|
       format.html { redirect_to jam_sessions_path }
